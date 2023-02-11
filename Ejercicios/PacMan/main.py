8a737382fe800b496ca736f8f4d430c8bece61f5
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-02-10 22:06:57 -0500

    Add the function to output the best score possible

--- Ejercicios/PacMan/main.py
+++ Ejercicios/PacMan/main.py
@@ -1,50 +1,85 @@
-##red
-#POSIBLES FUNCIONES
-#1. Construccion de tablero (Matriz)
-#2. Avanzar de izq a der o inversa (tal vez con número par o impar)
-#3. Buscar conteo del mayor numero de comida posible obtenida (es decir buscar el mayor número de comida posible entre fantasmas)
-#4. Verificacion de que el jugador solo ingrese un caracter por indice
-
-#POSIBLES METODOS
-#1. Condicional de pregunta si el usuario quiere seguir jugando
-#2. Condicional de verificacion si la primera entrada es unicamente un espacio ( '.' ) 
-#3. Condicional de que sean las entradas propuestas inicialmente por el jugador
-##
-
 ##orange
 #PREGUNTAR SI LA RESTRICCION DE SALIDA DEL USUARIO ES JUSTO CUANDO ACABE EL JUEGO O SI PUEDE SALIRSE SIN SIQUIERA COMENZAR
 ##
 
+##TIEMPO 4 HORAS
+
 import numpy as np
 
 def main():
-    createGameBoard()
+    play = 1
+    while play == 1:
+        gameBoard = createGameBoard()
+        print(theBestWay(gameBoard))
+        play =int(input("Do you want to continue playing?\n1.Yes\n2.No"))
 
 def createGameBoard():
-    n = int(input())
-    gameBoard = np.empty((n,n),str)
-    fillGameBoard(n,gameBoard)
+    condition = True
+    while condition == True:
+        n = int(input())
+        if 2 <= n <= 100:
+            condition = False
+            gameBoard = np.empty((n,n),str)
+        else:
+            print("Please, make sure your input is between 2 and 100.")
+    return fillGameBoard(n,gameBoard)
 
 def fillGameBoard(n,gameBoard):
+    firstEntry = True
     for i in range(n):
-        cond = True
-        while cond == True:
+        correctCharacter = True
+        condition = True
+        while condition == True:
             row = str(input())
-            cond = evaluateCorrectCharacters(row)
-            cond = True
-            if cond == False:
-                print("Please enter the correct charactes: '.' or 'A' or 'o'")
+            if firstEntry == True and row[0] != '.':
+                    print("Please verify that your first character is '.'.")
+            else:
+                if len(row) == n:
+                    correctCharacter = evaluateCorrectCharacters(row)
+                    if correctCharacter == False:
+                        print("Please enter the correct charactes: '.' or 'A' or 'o',")
+                    else:
+                        condition = False
+                        firstEntry = False
+                else:
+                    print("Please verify your entry, you bust be",n,"characters.")
         else:
             for j in range(n):
                 gameBoard[i][j] = row[j]
+    return gameBoard
 
 def evaluateCorrectCharacters(value):
+    condition = True
     empty = '.'
     ghost = 'A'
     meal = 'o'
     for i in range(len(value)):
-        if value[i] != empty or value[i] != ghost or value[i] != meal:
-            return False
+        if condition == True:
+            if value[i] != empty and value[i] != ghost and value[i] != meal: condition = False
+            else: condition = True
+    return condition
 
+def theBestWay(gameBoard):
+    count = 0
+    aux = 0
+    item = ''
+    n = len(gameBoard[0])
+    for i in range(n):
+        for j in range(n):
+            if (i+1)%2!=0:
+                item = gameBoard[i][j]
+            else:
+                item = gameBoard[i][n-1-j]
+            
+            if item == 'A':
+                if aux < count:
+                    aux = count    
+                count = 0
+            elif item == 'o':
+                count += 1
+    if aux < count:
+        aux = count
+    return aux
+            
 if __name__ == "__main__":
     main()
