d506e7f7f78dbd563cc2cbb521a3f6b1fc135680
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-02-20 00:52:40 -0500

    add partial solution of Voltereta with function that give the solution and add README empty

--- Ejercicios/Voltereta/main.py
+++ Ejercicios/Voltereta/main.py
@@ -1,9 +1,21 @@
 import numpy as np
 
-def voltearMatriz(matriz):
-    n = len(matriz[0])
-    auxMatriz = np.zeros((n,n))
-    for row in len(matriz[0]):
-        fila = matriz[row]
-        for i in n:
-            
+matriz = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+def main():
+    cantVueltas = int(input("Ingrese la cantidad de volteretas que quiere que de la matriz: "))
+    print(voltearMatriz(matriz,cantVueltas))
+
+def voltearMatriz(matriz,cantVueltas):
+    while cantVueltas != 0:
+        n = len(matriz[0])
+        auxMatriz = np.zeros((n,n),int)
+        for row in range(n):
+            fila = matriz[row]
+            auxMatriz[:,n-1-row] = [a for a in fila]
+        matriz = auxMatriz
+        cantVueltas -=1
+    return matriz
+    
+if __name__ == "__main__":
+    main()
