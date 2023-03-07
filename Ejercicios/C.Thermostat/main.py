7ed7e75beb22501568fc09e13536401c4b5ceda3
David Andrés Valencia García <david.andres.2801@gmail.com>
2023-03-06 22:16:00 -0500

    Problem Solved C.Thermostat

--- Ejercicios/C.Thermostat/main.py
+++ Ejercicios/C.Thermostat/main.py
@@ -1,50 +1,37 @@
-# Whuile para evaluar que no se salga de los rangos y la otra condicion
-# else para decir que es imposible (-1)
-# dos if para saber halalr el numero que sumarle o restarle si b es mayor o menor
-# dos if adicionales para saber si b esta por encima o debajo de a para sumarle o restarle el minimo
-# se necesita uan varible adicional "c"
-
 def main():
-    t = int(input())
-    
-    
-    for i in range(t):
-        l = int(input())
-        r = int(input())
-        x = int(input())
-        a = int(input())
-        b = int(input())
-        print(variateThemp(l, r, x, a, b))
-    
-    
+    condition = True
+    while condition == True:
+        t = int(input())
+        if 1<=t<=10**4:
+            condition = False
+            
+    while t!=0:
+        first_string = input().split(sep=None)
+        l = int(first_string[0])
+        r = int(first_string[1])
+        x = int(first_string[2])
+        second_string = input().split(sep=None)
+        a = int(second_string[0])
+        b = int(second_string[1])
+        if (1<=t<=10**4) and (-10**9<=l<=r<=10**9) and (1<=x<=10**9) and len(first_string)==3 and len(second_string)==2:
+            print(variateThemp(l, r, x, a, b))
+            t -=1
+
+
 def variateThemp(l,r,x,a,b):
-    c = 0
     cont = 0
-    while a!=b:
-        if abs(a-b)>=x:
-            cont+=1
-        elif a>b:
-            if cont == 0:
-                a = a - x
-                cont += 1
-            else:
-                while a != b:
-                    a = a + 1
-                else:
-                    cont += 1
-        elif a<b:
-            c = a + x
-            a = c
-            cont += 1
-            while a != b:
-                a = a - 1
-            else:
-                cont += 1
-        else:
-            cont = -1
-    
+    if a == b:
+        pass
+    elif abs(a-b)>=x:
+        cont = 1
+    elif (min(a,b) - l>=x) or (r - max(a,b)>=x):
+        cont = 2
+    elif ((r - b)>=x and (a - l)>=x) or ((r - a)>=x and (b - l)>=x):
+        cont = 3
+    else:
+        cont = -1
     return cont
-    
-    
+
+
 if __name__ == "__main__":
     main()
